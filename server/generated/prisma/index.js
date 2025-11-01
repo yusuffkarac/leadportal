e6670e86d2fceeac66f280d8f1c711b989b9e719
Yusuf Karaç <y.karac@simplydeliver.com>
2025-11-01 20:08:05 +0300

    feat: Update Prisma configuration and enhance database schema
    - Removed "postinstall" flag from Prisma configuration files (edge.js, index.js, wasm.js).
    - Added comprehensive database schema including new tables for User, Lead, Bid, and others.
    - Introduced ENUM types for LeadType and PaymentStatus.
    - Established foreign key constraints and unique indexes to ensure data integrity.
    - Enhanced migration script to reflect the current database structure and relationships.

--- server/generated/prisma/index.js
+++ server/generated/prisma/index.js
@@ -533,7 +533,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- server/generated/prisma/wasm.js
+++ server/generated/prisma/wasm.js
@@ -532,7 +532,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
